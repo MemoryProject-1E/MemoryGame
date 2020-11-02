210e61d64469fe591d26b9f5abda6fabf811eb7c
Meerten3 <msschoot3@gmail.com>
2020-11-02 17:16:12 +0100

    tweede thema geinstaleerd

--- MemoryGame/Models/Card.cs
+++ MemoryGame/Models/Card.cs
@@ -11,6 +11,7 @@ namespace MemoryGame.Models
 {
 	public class Card
 	{
+		private readonly Settings Config = new Settings();
 		private static readonly Dictionary<CardType, string> CardTypeImages = new Dictionary<CardType, string>
 		{
 			{ CardType.Fish1, "Fish1.jpg" },
@@ -29,6 +30,31 @@ namespace MemoryGame.Models
 
 		public Card(CardType type)
 		{
+			if (Config.ThemeCard == 2)
+			{
+				CardTypeImages.Clear();
+				CardTypeImages.Add(CardType.Fish1, "Aarde.jpg");
+				CardTypeImages.Add(CardType.Fish2, "Jupiter.jpg");
+				CardTypeImages.Add(CardType.Fish3, "Mars.png");
+				CardTypeImages.Add(CardType.Fish4, "Mercurius.jpg");
+				CardTypeImages.Add(CardType.Fish_5, "Neptunes.jpg");
+				CardTypeImages.Add(CardType.Fish_6, "Saturnus.jpg");
+				CardTypeImages.Add(CardType.Fish_7, "Uranus.jpg");
+				CardTypeImages.Add(CardType.Fish_8, "Venus.jpg");
+			}
+			else if (Config.ThemeCard == 1)
+			{
+				CardTypeImages.Clear();
+				CardTypeImages.Add(CardType.Fish1, "Fish1.jpg");
+				CardTypeImages.Add(CardType.Fish2, "Fish2.jpg");
+				CardTypeImages.Add(CardType.Fish3, "Fish3.png");
+				CardTypeImages.Add(CardType.Fish4, "Fish4.jpg");
+				CardTypeImages.Add(CardType.Fish_5, "Fish_5.jpg");
+				CardTypeImages.Add(CardType.Fish_6, "Fish_6.jpg");
+				CardTypeImages.Add(CardType.Fish_7, "Fish_7.jpg");
+				CardTypeImages.Add(CardType.Fish_8, "Fish_8.jpg");
+			}
+
 			Type = type;
 
 			Element = new Button()
